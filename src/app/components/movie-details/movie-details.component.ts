4dc780aa9c0a3bfa48fab1afbbeef7921b751466
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-06-12 22:28:53 +0300

    part1

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -9,10 +9,12 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class MovieDetailsComponent implements OnInit {
   movie: Movie;
+  img: string;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     this.activatedRoute.params.subscribe(params => {
       this.movie = this.router.getCurrentNavigation()?.extras.state as Movie;
+      this.img = 'https://image.tmdb.org/t/p/original' + this.movie.poster_path;
     })
   }
 
